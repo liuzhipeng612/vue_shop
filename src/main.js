a8a413c30c4d54b3fdb056f70762e8605e947258
PysDev <liuzhipeng612@outlook.com>
2020-08-08 02:01:56 +0800

    添加导入axios插件文本说明

--- src/main.js
+++ src/main.js
@@ -4,9 +4,12 @@ import router from './router'
 import './plugins/element.js'
 import './assets/css/global.css'
 import './assets/fonts/iconfont.css'
+// 导入axios发起api请求
 import axios from 'axios'
-
+// 设置api请求基本域名
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// 配置Vue原属性prototype的$http采用axios插件
+// 将axios关联到Vue的原型对象$http上
 Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
