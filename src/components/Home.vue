93e3b56807d0458085e4d971ed178af5aa653d0d
PysDev <liuzhipeng612@outlook.com>
2020-08-08 19:20:18 +0800

    完成登录页面开发

---
+++ src/components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <!--  监听退出按钮，点击按钮调用logout方法-->
+  <el-button @click="logout" type="info">退出</el-button>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  methods: {
+    logout () {
+      // 使用window DOM调用seesionStorage的clear方法清空token
+      window.sessionStorage.clear()
+      // 使用router路由，push到对应页面
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 // 导入需要使用路由展示的组件
 import Login from '@/components/Login'
+import Home from '@/components/Home'
 
 Vue.use(VueRouter)
 
@@ -14,10 +15,28 @@ const routes = [{
 }, {
   path: '/login',
   component: Login
+}, {
+  path: '/home',
+  component: Home
 }]
 
 const router = new VueRouter({
   routes
+})
+// 挂载路由导航守卫
+router.beforeEach((to, from, next) => {
+  // to 将要访问的路径
+  // from 代表从哪个路径跳转而来
+  // next 是一个函数，标识放行
+  // next() 代表放行, next('/login') 代表强制跳转
+  // 如果用户访问登录页，直接放行
+  if (to.path === '/login') return next()
+  // 从sessionStorage中获取并保存token值
+  const tokenStr = window.sessionStorage.getItem('token')
+  // 如果没有token，强制跳转登录页
+  if (!tokenStr) return next('/login')
+  // 否则放行
+  next()
 })
 
 export default router
